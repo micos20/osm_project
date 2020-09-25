7877339be42e58d66a967ecb243031a11f4603e3
Michael Wrzos <michael.wrzos@gmx.de>
2020-09-25 18:35:53 +0200

    Add OSM db

--- scripts/quick_parse_osm.py
+++ scripts/quick_parse_osm.py
@@ -4,6 +4,7 @@
 Count number of different tags in osm data file
 """
 import xml.etree.cElementTree as ET
+import wrangle_hlp as wh
 
 def count_tags(filename):
     tags = {}       # dict to store the tags {tag: number}
@@ -13,4 +14,17 @@ def count_tags(filename):
         else:
             tags[elm.tag] = 1
     return tags
+
+def count_ways_tags(osm_file):
+    way_count = 0
+    for way in wh.get_element(osm_file, tags=('way')):
+        for tag in way.iter('tag'):
+            way_count += 1
+    return way_count
     
+def count_nodes_tags(osm_file):
+    node_count = 0
+    for node in wh.get_element(osm_file, tags=('node')):
+        for tag in node.iter('tag'):
+            node_count += 1
+    return node_count
